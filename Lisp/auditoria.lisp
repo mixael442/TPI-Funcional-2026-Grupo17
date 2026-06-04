44fd6473471f8ff9f518d63da8f765898a45510a
EDUARDO JOSUE <aduardojoss@gmail.com>
2026-06-04 16:57:14 -0300

    Enhance function comments and documentationagregue una par de funciones mas para el historial
    Added function documentation and improved comments.

--- Lisp/auditoria.lisp
+++ Lisp/auditoria.lisp
@@ -1,10 +1,11 @@
 ;AUDITORÍA - REQUERIMIENTO 3
 
-;registrar-evento
+;FUNCION: registrar-evento
 ;NATURALEZA: Pura
 ;DESCRIPCIÓN:
 ;Agrega un nuevo evento al historial y devuelve
 ;un historial actualizado.
+;IMPACTO: no destructiva
 (defun registrar-evento
        (timestamp estado-anterior estado-nuevo historial)
 
@@ -12,11 +13,37 @@
    (list timestamp estado-anterior estado-nuevo)
    historial))
 
+; FUNCION: mostrar-evento
+; NATURALEZA: Impura
+;DESCRIPCION:  Muestra un único evento por pantalla.
+;IMPACTO:no destructiva
+(defun mostrar-evento (evento)
 
-;escribir-eventos
+  (format t
+          "Tiempo ~A: la luz ha cambiado de ~A a ~A~%"
+          (car evento)
+          (cadr evento)
+          (caddr evento)))
+
+;FUNCION: mostrar-historial
+; NATURALEZA: Impura
+; Recorre recursivamente el historial
+; y muestra cada evento.
+;IMPACTO: nos destructiva
+(defun mostrar-historial (historial)
+
+  (cond
+    ((null historial) nil)
+
+    (t
+     (mostrar-evento (car historial))
+     (mostrar-historial (cdr historial)))))
+
+
+;FUNCION: escribir-eventos
 ;NATURALEZA: Impura
-;DESCRIPCIÓN:
-;Escribe los eventos del historial en un stream.
+;DESCRIPCIÓN: Escribe los eventos del historial en un stream.
+;IMPACTO: no destructiva
 (defun escribir-eventos (historial stream)
 
   (cond
@@ -37,10 +64,11 @@
 
 
 
-;informe
+;FUNCION: informe
 ;NATURALEZA: Impura
 ;DESCRIPCIÓN:
 ;Genera un archivo con el historial de eventos.
+;IMPACTO: destructiva
 (defun informe (historial)
 
   (with-open-file
