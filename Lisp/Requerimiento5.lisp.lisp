cd0c99bd7229f132f8824e46a75a2111e752b4ab
vanessagonz <82358656+vanessagonz@users.noreply.github.com>
2026-06-06 18:15:14 -0300

    Add files via upload

---
+++ Lisp/Requerimiento5.lisp.lisp
@@ -0,0 +1,16 @@
+;;;; ============================================================
+;;;; REQUERIMIENTO 5: PLANIFICACIÓN TEMPORAL
+;;;; FUNCIÓN: ciclos-por-tiempo
+;;;; Entrada: minutos
+;;;; Salida: número de ciclos completos en ese período
+;;;; ============================================================
+
+(defun ciclos-por-tiempo (minutos)
+  (let ((duracion-ciclo 216)   ; 90 + 6 + 120 segundos
+        (segundos-totales (* minutos 60)))
+    (floor segundos-totales duracion-ciclo)))
+
+(format t "~a~%" (ciclos-por-tiempo 15))
+(format t "~a~%" (ciclos-por-tiempo 60))
+(format t "~a~%" (ciclos-por-tiempo 3))
+(format t "~a~%" (ciclos-por-tiempo 1))
