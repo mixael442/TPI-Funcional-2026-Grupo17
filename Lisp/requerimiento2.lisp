b60477bd709dc3a49f052e5eb8ef7fbd4ecd79e0
EDUARDO JOSUE <aduardojoss@gmail.com>
2026-06-12 05:45:23 -0300

    Refactor comparar-rem function for traffic light logic

--- Lisp/requerimiento2.lisp
+++ Lisp/requerimiento2.lisp
@@ -13,14 +13,29 @@
     (compararRem (rem tiempo (+ rojo verde amarillo)) rojo verde amarillo)
 )
 
-(defun comparar-rem(resto rojo verde amarillo) 
-    (cond
-        ((< resto rojo) 'en-rojo)
-        ((< resto (+ amarillo rojo)) 'en-amarillo)
-        ((>= resto (+ amarillo rojo)) 'en-verde)
-        (t "error")
-    )
-)
+(defun compararRem (resto rojo verde amarillo)
 
+  (cond
+
+    ((< resto rojo)
+     'en-rojo)
+
+    ((< resto (+ rojo 3))
+     'rojo-intermitente)
+
+    ((< resto (+ rojo 3 verde))
+     'en-verde)
+
+    ((< resto (+ rojo 3 verde 3))
+     'verde-intermitente)
+
+    ((< resto (+ rojo 3 verde 3 amarillo))
+     'en-amarillo)
+
+    ((< resto (+ rojo 3 verde 3 amarillo 3))
+     'amarillo-intermitente)
+
+    (t
+     "error")))
 
 
