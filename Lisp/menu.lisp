f2662b7b224b71b1d4a92704de9d7a1e2a6a9524
EDUARDO JOSUE <aduardojoss@gmail.com>
2026-06-12 05:38:29 -0300

    Add main menu for traffic light system

---
+++ Lisp/menu.lisp
@@ -0,0 +1,66 @@
+; MENU PRINCIPAL
+
+(defun mostrar-menu ()
+
+  (format t "~%")
+  (format t " SISTEMA DE SEMAFOROS INTELIGENTES~%")
+  (format t "1 - Probar transicion~%")
+  (format t "2 - Consultar timer~%")
+  (format t "3 - Mostrar historial~%")
+  (format t "4 - Duracion de ciclo~%")
+  (format t "5 - Recomendacion de ciclo~%")
+  (format t "6 - Ciclos por tiempo~%")
+  (format t "0 - Salir~%")
+  (format t "Seleccione una opcion: ")
+  (read))
+
+(defun ejecutar-menu ()
+  (let ((opcion (mostrar-menu)))
+    (cond
+      ; SALIR
+      ((= opcion 0)
+       (format t "~%Finalizando programa...~%"))
+; REQ 1
+      ((= opcion 1)
+       (format t "~%Color actual: ")
+       (let ((actual (read)))
+         (format t "Cambiar a: ")
+         (let ((nuevo (read)))
+           (format t "~A~%"
+                   (transicion actual nuevo))))
+       (ejecutar-menu))
+; REQ 2
+      ((= opcion 2)
+       (format t "~%Tiempo Unix: ")
+       (let ((tiempo (read)))
+         (format t "~A~%"
+                 (calTimer tiempo)))
+       (ejecutar-menu))
+; REQ 3
+      ((= opcion 3)
+       (format t "~%Ingrese variable historial: ")
+       (let ((historial (read)))
+         (mostrar-historial historial))
+       (ejecutar-menu))
+; REQ 4
+      ((= opcion 4)
+       (format t "~%Duracion ciclo: ~A segundos~%"
+               (duracion-ciclo))
+       (ejecutar-menu))
+ ;REQ 4b
+      ((= opcion 5)
+       (format t "~%~A~%"
+               (recomendacion-ciclo
+                (duracion-ciclo)))
+       (ejecutar-menu))
+; REQ 5
+      ((= opcion 6)
+       (format t "~%Minutos: ")
+       (let ((minutos (read)))
+         (format t "~A~%"
+                 (ciclos-por-tiempo minutos)))
+       (ejecutar-menu))
+      ;; ERROR
+      (t
+       (format t "~%Opcion invalida~%")
+       (ejecutar-menu)))))
