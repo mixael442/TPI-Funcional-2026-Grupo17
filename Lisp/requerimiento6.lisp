2d1c6b33a24dad12af1dcd7b52f10096e2b9e9f1
EDUARDO JOSUE <aduardojoss@gmail.com>
2026-06-13 21:45:08 -0300

    Refactor porcentaje-color to include intermitente colors
    Refactor 'porcentaje-color' function to include 'intermitente' color calculations and improve readability.

--- Lisp/requerimiento6.lisp
+++ Lisp/requerimiento6.lisp
@@ -1,20 +1,46 @@
 (load "funcionesAux.lisp")
+
+
+; FUNCIÓN: porcentaje-color
+; NATURALEZA: Pura.
+; ESTRATEGIA: Composición funcional y operaciones aritméticas.
+; IMPACTO: No destructiva
+
 (defun porcentaje-color ()
-    (let* ((rojo (cdr (obtener-color :rojo))) 
-           (verde (cdr (obtener-color :verde)))
-           (amarillo (cdr (obtener-color :amarillo)))
-           (ciclo (+ rojo verde amarillo))
-           (ciclos (floor 3600 ciclo))
-           (resto (rem 3600 ciclo)))
-
+    (let* ((rojo (+ (cdr (obtenerColor :rojo))
+                  (cdr (obtenerColor :intermitente))))
+         (verde (+ (cdr (obtenerColor :verde))
+                   (cdr (obtenerColor :intermitente))))
+         (amarillo (+ (cdr (obtenerColor :amarillo))
+                      (cdr (obtenerColor :intermitente))))
+         (ciclo (+ rojo verde amarillo))
+         (ciclos (floor 3600 ciclo))
+         (resto (rem 3600 ciclo)))
           
-        (list (cons 'rojo (calcular rojo (calcular-rem resto rojo verde amarillo 'rojo) ciclos)) 
-              (cons 'verde (calcular verde (calcular-rem resto rojo verde amarillo 'verde) ciclos))
-              (cons 'amarillo (calcular amarillo (calcular-rem resto rojo verde amarillo 'amarillo) ciclos)) 
-    )
-))
+          (list
 
-(defun calcular-rem (resto rojo verde amarillo actual)
+     (cons 'rojo
+           (calcular
+            rojo
+            (calcular-rest resto rojo verde amarillo 'rojo)
+            ciclos))
+     (cons 'verde
+           (calcular
+            verde
+            (calcular-rest resto rojo verde amarillo 'verde)
+            ciclos))
+     (cons 'amarillo
+           (calcular
+            amarillo
+            (calcular-rest resto rojo verde amarillo 'amarillo)
+            ciclos))
+     )))
+
+; FUNCIÓN: calcular-rest
+; NATURALEZA: Pura
+; ESTRATEGIA: Condicional mediante evaluación de casos.
+; IMPACTO: No destructiva
+(defun calcular-rest (resto rojo verde amarillo actual)
     (cond
         ((equal actual 'rojo)(min resto rojo))
         ((equal actual 'verde)(max (min (- resto rojo) verde) 0))
@@ -22,7 +48,10 @@
         (t 0)
     )
 )
-
+; FUNCIÓN: calcular
+; NATURALEZA: Pura
+; ESTRATEGIA: Operación aritmética.
+; IMPACTO: No destructiva
 (defun calcular (color resto ciclos)
     (* 100.0 (/ (+ (* color ciclos) resto) 3600))
 )
